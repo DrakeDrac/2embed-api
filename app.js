15a7f4f286b49cddda2b9b681aab08d0ed642d47
DrakeDrac <50480704+DrakeDrac@users.noreply.github.com>
2023-02-04 21:31:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routers_1 = require("./routers");
+class App {
+    constructor() {
+        this.app = (0, express_1.default)();
+        this.app.use((0, cors_1.default)({ origin: true }));
+        this.routes();
+    }
+    listen() {
+        const PORT = process.env.PORT || 8080;
+        this.app.listen(PORT, () => {
+            console.log(`App listening on the port ${PORT}`);
+        });
+    }
+    routes() {
+        (0, routers_1.initRoutes)(this.app);
+    }
+}
+exports.default = App;
+//# sourceMappingURL=app.js.map
