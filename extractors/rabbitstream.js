1f7abd6abae00eb35b50646dbd3196bc1ba61465
DrakeDrac <50480704+DrakeDrac@users.noreply.github.com>
2023-02-04 22:04:59 +0530

    Update rabbitstream.js

--- extractors/rabbitstream.js
+++ extractors/rabbitstream.js
@@ -59,7 +59,7 @@ function rabbitstreamExtract(url) {
                 return (JSON.parse(bytes.toString(crypto_js_1.default.enc.Utf8)));
             }
             catch (e) {
-                console.log("Dokicloud key failed to decrypt source");
+                console.log("Dokicloud key failed to decrypt source "+e.message);
             }
             try { // Rabbitstream
                 let decryptionKey = (yield axios_1.default.get('https://raw.githubusercontent.com/enimax-anime/key/e6/key.txt')).data;
@@ -67,7 +67,7 @@ function rabbitstreamExtract(url) {
                 return (JSON.parse(bytes.toString(crypto_js_1.default.enc.Utf8)));
             }
             catch (e) {
-                console.log("Rabbitstream key failed to decrypt source");
+                console.log("Rabbitstream key failed to decrypt source "+e.message);
             }
         });
         data.sources = yield decryptSource(data.sources);
