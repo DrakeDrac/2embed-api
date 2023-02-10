14c0c60aa4e95e883d15b7510e263d9967f35677
DrakeDrac <50480704+DrakeDrac@users.noreply.github.com>
2023-02-10 18:13:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const routers_1 = require("./routers");
+const apicache = __importDefault(require("apicache"));
 class App {
-    constructor() {
-        this.app = (0, express_1.default)();
-        this.app.use((0, cors_1.default)({ origin: true }));
-        this.routes();
-    }
-    listen() {
-        const PORT = process.env.PORT || 8080;
-        this.app.listen(PORT, () => {
-            console.log(`App listening on the port ${PORT}`);
-        });
-    }
-    routes() {
-        (0, routers_1.initRoutes)(this.app);
-    }
+  constructor() {
+    this.cache = apicache.default.options({
+      debug: true,
+      trackPerformance: false,
+    }).middleware;
+    this.app = (0, express_1.default)();
+    this.app.use((0, cors_1.default)({ origin: true }));
+    this.app.use(this.cache());
+    this.routes();
+  }
+  listen() {
+    const PORT = process.env.PORT || 8080;
+    this.app.listen(PORT, () => {
+      console.log(`App listening on the port ${PORT}`);
+    });
+  }
+  routes() {
+    (0, routers_1.initRoutes)(this.app);
+  }
 }
 exports.default = App;
 //# sourceMappingURL=app.js.map
