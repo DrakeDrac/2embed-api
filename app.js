6d1a2bd9e2aadb87a192b314d2853cdc3c49d5e7
DrakeDrac <50480704+DrakeDrac@users.noreply.github.com>
2023-02-15 15:16:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const routers_1 = require("./routers");
-const apicache = __importDefault(require("apicache"));
+const apicache = __importDefault(require("./apicache"));
 class App {
   constructor() {
     this.cache = apicache.default.options({
-      debug: true,
+      debug: false,
       trackPerformance: false,
     }).middleware;
     this.app = (0, express_1.default)();
     this.app.use((0, cors_1.default)({ origin: true }));
-    this.app.use(this.cache());
+    this.app.use(this.cache("5 seconds"));
     this.routes();
   }
   listen() {
